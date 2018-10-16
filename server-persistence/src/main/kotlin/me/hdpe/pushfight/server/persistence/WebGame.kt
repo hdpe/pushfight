4420348b8bdd67534622183d270bd7361ffe18da
Ryan Pickett <ryan@hdpe.me>
2018-10-16 08:32:31 +0100

    JsonIgnore extraneous props from WebGame

--- server-persistence/src/main/kotlin/me/hdpe/pushfight/server/persistence/WebGame.kt
+++ server-persistence/src/main/kotlin/me/hdpe/pushfight/server/persistence/WebGame.kt
@@ -1,10 +1,19 @@
 package me.hdpe.pushfight.server.persistence
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import me.hdpe.pushfight.engine.GameState
 
-class WebGame(val id: String, val player1AccountId: String, val player2AccountId: String, val gameState: GameState,
-              val victorAccountId: String?) {
+class WebGame(
+        val id: String,
 
+        @get:JsonIgnore val player1AccountId: String,
+
+        @get:JsonIgnore val player2AccountId: String,
+
+        val gameState: GameState,
+
+        @get:JsonIgnore val victorAccountId: String?
+) {
     fun getPlayer(number: Int): WebPlayer {
         return with(gameState.config) { if (number == 1) player1 else player2 } as WebPlayer
     }
