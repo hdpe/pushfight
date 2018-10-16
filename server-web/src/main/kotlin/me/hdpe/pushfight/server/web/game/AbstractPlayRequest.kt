bdfa8185291da6f53c3b718b584a86cac40cf328
Ryan Pickett <ryan@hdpe.me>
2018-10-16 08:33:01 +0100

    Use @Min/Max instead of Range
    @Range is not working with springfox.

--- server-web/src/main/kotlin/me/hdpe/pushfight/server/web/game/AbstractPlayRequest.kt
+++ server-web/src/main/kotlin/me/hdpe/pushfight/server/web/game/AbstractPlayRequest.kt
@@ -1,12 +1,12 @@
 package me.hdpe.pushfight.server.web.game
 
-import org.hibernate.validator.constraints.Range
-import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
 import javax.validation.constraints.NotNull
-import javax.validation.constraints.Size
 
 abstract class AbstractPlayRequest(
         @get:NotNull(message = "{NotNull.playerNumber}")
-        @get:Range(min = 1, max = 2, message = "{Range.playerNumber}")
+        @get:Min(value = 1, message = "{Range.playerNumber}")
+        @get:Max(value = 2, message = "{Range.playerNumber}")
         var playerNumber: Int?
 )
