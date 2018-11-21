7a354e04e28b519d623422dd3f504addfffbf032
Ryan Pickett <ryan@hdpe.me>
2018-11-21 10:42:33 +0000

    Fix Swagger model

---
+++ server-web/src/main/kotlin/me/hdpe/pushfight/server/web/swagger/ModelEnhancingSwaggerPlugin.kt
@@ -0,0 +1,73 @@
+package me.hdpe.pushfight.server.web.swagger
+
+import com.fasterxml.classmate.TypeResolver
+import me.hdpe.pushfight.engine.Piece
+import me.hdpe.pushfight.engine.Square
+import org.springframework.stereotype.Component
+import springfox.documentation.builders.ModelPropertyBuilder
+import springfox.documentation.schema.ModelProperty
+import springfox.documentation.schema.ResolvedTypes
+import springfox.documentation.schema.TypeNameExtractor
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.schema.ModelBuilderPlugin
+import springfox.documentation.spi.schema.ModelPropertyBuilderPlugin
+import springfox.documentation.spi.schema.contexts.ModelContext
+import springfox.documentation.spi.schema.contexts.ModelPropertyContext
+
+/**
+ * We need to make some manual changes to the Swagger model to bring it in line with the customisations in the
+ * Jackson module.
+ */
+@Component
+class ModelEnhancingSwaggerPlugin(val typeResolver: TypeResolver, val typeNameExtractor: TypeNameExtractor) :
+        ModelBuilderPlugin, ModelPropertyBuilderPlugin {
+
+    override fun apply(context: ModelContext) {
+        if (context.type == typeResolver.resolve(Piece::class.java)) {
+            context.builder.properties(mapOf(
+                    Pair("type", toModelProperty(ModelPropertyBuilder()
+                            .name("type")
+                            .required(true)
+                            .type(typeResolver.resolve(java.lang.String::class.java)), context)
+                    ),
+                    Pair("hatted", toModelProperty(ModelPropertyBuilder()
+                            .name("hatted")
+                            .type(typeResolver.resolve(java.lang.Boolean::class.java)), context)
+                    )
+            ))
+        }
+
+        if (context.type == typeResolver.resolve(Square::class.java)) {
+            context.builder.properties(mapOf(
+                    Pair("type", toModelProperty(ModelPropertyBuilder()
+                            .name("type")
+                            .required(true)
+                            .type(typeResolver.resolve(java.lang.String::class.java)), context)
+                    ),
+                    Pair("rail", toModelProperty(ModelPropertyBuilder()
+                            .name("rail")
+                            .type(typeResolver.resolve(java.lang.String::class.java)), context)
+                    ),
+                    Pair("piece", toModelProperty(ModelPropertyBuilder()
+                            .name("piece")
+                            .type(typeResolver.resolve(Piece::class.java)), context))
+            ))
+        }
+    }
+
+    override fun apply(context: ModelPropertyContext) {
+        val propertyName = context.beanPropertyDefinition?.get()?.fullName?.simpleName
+
+        if (propertyName == "owner") {
+            context.builder.type(typeResolver.resolve(java.lang.Integer::class.java))
+        }
+    }
+
+    override fun supports(delimiter: DocumentationType): Boolean = (delimiter == DocumentationType.SWAGGER_2)
+
+    private fun toModelProperty(builder: ModelPropertyBuilder, ctx: ModelContext): ModelProperty {
+        val property = builder.build()
+        property.updateModelRef(ResolvedTypes.modelRefFactory(ctx, typeNameExtractor))
+        return property
+    }
+}
