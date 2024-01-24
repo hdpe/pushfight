32f43bb0a25baa54e9a40f21fbb8570e9260ba44
Ryan Pickett <hdpe@users.noreply.github.com>
2024-01-24 22:48:05 +0000

    Fix username conflict error reporting

--- server-persistence/src/main/kotlin/me/hdpe/pushfight/server/persistence/account/AccountEntityConstants.kt
+++ server-persistence/src/main/kotlin/me/hdpe/pushfight/server/persistence/account/AccountEntityConstants.kt
@@ -1,3 +1,3 @@
 package me.hdpe.pushfight.server.persistence.account
 
-const val USERNAME_UNIQUE_CONSTRAINT_NAME = "uc_accounts_username"
+const val USERNAME_UNIQUE_CONSTRAINT_NAME = "uc_accounts_normalized_username"
